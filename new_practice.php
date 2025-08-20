c420c1c3098b7054a0840d5be4708ed32e3def14
Ayushi-dahiya <146051693+Ayushi-dahiya@users.noreply.github.com>
2025-08-20 21:00:53 +0530

    Add files via upload

---
+++ new_practice.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <body>
+        <script type="text/javascript">
+            var book="maths";
+            if(book=="history")
+        {document.write("<b> history book</b>");
+
+        }
+        elseif( book=="maths")
+        {document.write("<b> maths books</b>");
+        }
+        else(book="geo")
+        {document.write("<b>geo</b>");}
+</script>
+    </body>
+    </html>
